fdc72c217e83c2d084131ce713715828c47c3d40
manh12a5 <73921743+manh12a5@users.noreply.github.com>
2021-04-22 21:20:38 +0700

    Tạo Query Search ở Repo

--- src/main/java/com/example/demo/repository/SongRepository.java
+++ src/main/java/com/example/demo/repository/SongRepository.java
@@ -2,8 +2,17 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SongRepository extends JpaRepository<Song, Long> {
+    @Query(value = "select * from song order by creation_time desc limit 10", nativeQuery = true)
+    List<Song> findAllByCreationTimeOrderByCreationTime();
+    @Query(value = "select * from song order by number_of_view desc limit 10", nativeQuery = true)
+    List<Song> findAllByNumberOfViewOrderByNumberOfView();
+
+    List<Song> findAllByNameSong(String nameSong);
 }
