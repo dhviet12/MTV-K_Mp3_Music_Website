82936f44e71b2fcde2e42ef5c9cf997b3dd22b99
manh12a5 <73921743+manh12a5@users.noreply.github.com>
2021-04-26 08:44:37 +0700

    Fix Config

--- src/main/java/com/example/demo/model/Song.java
+++ src/main/java/com/example/demo/model/Song.java
@@ -17,10 +17,6 @@ public class Song {
     private String fileMp3;
     private String avatar;
     private String author;
-    @Column(nullable = false)
-    private Date creationTime;
-    @Column(nullable = false)
-    private Long numberOfView;
 
     @ManyToMany(mappedBy = "songs")
     private List<Singer> singer;
@@ -37,15 +33,13 @@ public class Song {
     public Song() {
     }
 
-    public Song(Long id, String nameSong, String description, String fileMp3, String avatar, String author, Date creationTime, Long numberOfView, List<Singer> singer, User poster, Category category, List<Like> likes, List<CommentOfSong> comments) {
+    public Song(Long id, String nameSong, String description, String fileMp3, String avatar, String author, List<Singer> singer, User poster, Category category, List<Like> likes, List<CommentOfSong> comments) {
         this.id = id;
         this.nameSong = nameSong;
         this.description = description;
         this.fileMp3 = fileMp3;
         this.avatar = avatar;
         this.author = author;
-        this.creationTime = creationTime;
-        this.numberOfView = numberOfView;
         this.singer = singer;
         this.poster = poster;
         this.category = category;
@@ -99,22 +93,6 @@ public class Song {
 
     public void setAuthor(String author) {
         this.author = author;
-    }
-
-    public Date getCreationTime() {
-        return creationTime;
-    }
-
-    public void setCreationTime(Date creationTime) {
-        this.creationTime = creationTime;
-    }
-
-    public Long getNumberOfView() {
-        return numberOfView;
-    }
-
-    public void setNumberOfView(Long numberOfView) {
-        this.numberOfView = numberOfView;
     }
 
     public List<Singer> getSinger() {
