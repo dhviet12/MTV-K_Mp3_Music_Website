516d2fee5e667baa1913db5e73b88239de8ed5d2
Your Name <you@example.com>
2021-04-27 21:48:01 +0700

    tao service

--- src/main/java/com/example/demo/repository/comment/CommentPlayListRepository.java
+++ src/main/java/com/example/demo/repository/comment/CommentPlayListRepository.java
@@ -1,8 +1,15 @@
 package com.example.demo.repository.comment;
 
 import com.example.demo.model.comment.CommentOfPlayList;
+import com.example.demo.model.comment.CommentOfSong;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CommentPlayListRepository extends CrudRepository<CommentOfPlayList, Long> {
-
+//    @Query(value = "select * from comment_of_song where song_id = ?", nativeQuery = true)
+    List<CommentOfPlayList> getAllByPlayListId(Long id);
 }
