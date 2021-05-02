76181e7b60c1c48d40ab9ce7658cbe2e11f1275e
Your Name <you@example.com>
2021-05-02 13:46:50 +0700

    model like playlist

---
+++ src/main/java/com/example/demo/model/LikePlayList.java
@@ -0,0 +1,51 @@
+package com.example.demo.model;
+
+import com.example.demo.model.user.User;
+
+import javax.persistence.*;
+
+@Entity
+public class LikePlayList {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private PlayList playList;
+
+    public LikePlayList() {
+    }
+
+    public LikePlayList(Long id, User user, PlayList playList) {
+        this.id = id;
+        this.user = user;
+        this.playList = playList;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public PlayList getPlayList() {
+        return playList;
+    }
+
+    public void setPlayList(PlayList playList) {
+        this.playList = playList;
+    }
+}
