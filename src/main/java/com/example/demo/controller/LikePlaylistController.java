b7751a048cb6339f2870cb4cf1870f080f2e72d8
dhviet12 <73893345+dhviet12@users.noreply.github.com>
2021-05-03 10:19:37 +0700

    tao controller like playlist

--- src/main/java/com/example/demo/controller/LikePlaylistController.java
+++ src/main/java/com/example/demo/controller/LikePlaylistController.java
@@ -1,66 +1,61 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.CommentOfSong;
+import com.example.demo.model.LikePlaylist;
 import com.example.demo.model.LikeSong;
+import com.example.demo.model.PlayList;
 import com.example.demo.model.Song;
 import com.example.demo.model.user.User;
 import com.example.demo.model.user.UserPrinciple;
 import com.example.demo.model.user.response.ResponseMessage;
-import com.example.demo.service.SongServiceImp;
-import com.example.demo.service.likeSong.LikeSongService;
+import com.example.demo.service.likePlaylist.LikePlaylistService;
+import com.example.demo.service.playlist.PlaylistService;
 import com.example.demo.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/likeSong")
-public class LikeSongController {
+@RequestMapping("/likePlaylist")
+public class LikePlaylistController {
     @Autowired
-    LikeSongService likeSongService;
+    LikePlaylistService likePlaylistService;
+
+    @Autowired
+    PlaylistService playlistService;
 
     @Autowired
     UserService userService;
 
-    @Autowired
-    SongServiceImp songService;
-
     @GetMapping("/{id}")
     public ResponseEntity<?> likeSongModel(@PathVariable Long id) {
-        if (likeSongService.findById(id) == null) {
+        if (likePlaylistService.findById(id) == null) {
             return new ResponseEntity<>(new ResponseMessage("Not found"), HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(likeSongService.findById(id), HttpStatus.OK);
+        return new ResponseEntity<>(likePlaylistService.findById(id), HttpStatus.OK);
     }
 
     @PostMapping("/like")
-    public ResponseEntity<?> like(@RequestBody LikeSong likeSong) {
-        return new ResponseEntity<>(likeSongService.save(likeSong), HttpStatus.CREATED);
+    public ResponseEntity<?> like(@RequestBody LikePlaylist likePlaylist) {
+        return new ResponseEntity<>(likePlaylistService.save(likePlaylist), HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/unlike/{sid}")
-    public ResponseEntity<?> unlike(@PathVariable Long sid) {
+    @DeleteMapping("/unlike/{pid}")
+    public ResponseEntity<?> unlike(@PathVariable Long pid) {
 
+        PlayList playList = playlistService.findById(pid);
 
-        Song song = songService.findById(sid);
-
-//        if (likeSong.getUser() == currentUser ) {
-//            likeSongService.delete(likeSong.getId());
-//            return new ResponseEntity<>(new ResponseMessage("Unlike"),HttpStatus.OK);
-//        }
-        if (song != null) {
-            likeSongService.deleteLikeSong(sid);
+        if (playList != null ) {
+            likePlaylistService.deleteLikePlaylist(pid);
             return new ResponseEntity<>(new ResponseMessage("Unlike"), HttpStatus.OK);
         }
 
         return new ResponseEntity<>(new ResponseMessage("Failed"), HttpStatus.BAD_REQUEST);
     }
+
+
+
 }
---
+++ src/main/java/com/example/demo/service/likePlaylist/ILikePlaylistService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service.likePlaylist;
+
+import com.example.demo.model.LikePlaylist;
+
+
+public interface ILikePlaylistService {
+    LikePlaylist save (LikePlaylist likePlaylist);
+    void delete(Long id);
+
+    LikePlaylist findById(Long id);
+}
